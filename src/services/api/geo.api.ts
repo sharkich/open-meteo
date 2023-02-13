a1ab6562d1e3ecb42ca9846dca772cb86a1df4db
Artem Koziar <koziar.artem@gmail.com>
2023-02-13 13:38:48 +0200

    add weather api

--- src/services/api/geo.api.ts
+++ src/services/api/geo.api.ts
@@ -1,28 +1,26 @@
 import axios from 'axios';
 
-export namespace GeoApi {
-  const URL = 'https://geocoding-api.open-meteo.com/v1/search';
+const URL = 'https://geocoding-api.open-meteo.com/v1/search';
 
-  export interface IGeoDTO {
-    admin1: string;
-    admin1_id: number;
-    country: string;
-    country_code: string;
-    country_id: number;
-    elevation: number;
-    feature_code: string;
-    id: number;
-    latitude: number;
-    longitude: number;
-    name: string;
-    population: number;
-    timezone: string;
-  }
-
-  export const fetchGeo = async (name: string) => {
-    const query = new URLSearchParams({ name });
-    const response = await axios.get<IGeoDTO>(`${URL}?${query}`);
-
-    return response.data;
-  };
+export interface IGeoDTO {
+  admin1: string;
+  admin1_id: number;
+  country: string;
+  country_code: string;
+  country_id: number;
+  elevation: number;
+  feature_code: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  population: number;
+  timezone: string;
 }
+
+export const fetchGeo = async (name: string) => {
+  const query = new URLSearchParams({ name });
+  const response = await axios.get<IGeoDTO>(`${URL}?${query}`);
+
+  return response.data;
+};
